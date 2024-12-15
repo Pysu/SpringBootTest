74ee6dfc5fc3a368e32ab3a14b192c314a94643b
suyoupeng <suyoupeng@agree.com.cn>
2024-12-15 21:59:08 +0800

    第二次修改

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -7,6 +7,6 @@ package src.main.java;
  */
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Hello World");
     }
 }

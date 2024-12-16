2587c02e345318d065b9ff4fdb36f1d2f939af03
suyoupeng <suyoupeng@agree.com.cn>
2024-12-16 11:15:16 +0800

    新增TestMain

---
+++ src/main/java/TestMain.java
@@ -0,0 +1,12 @@
+package src.main.java;
+
+/**
+ * @author suyp
+ * @date 2024-12-16
+ * @description
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

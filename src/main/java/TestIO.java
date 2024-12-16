46c9e902b981bc0ec703cb2d0eabf1bfd03cc68f
suyoupeng <suyoupeng@agree.com.cn>
2024-12-16 11:18:33 +0800

    新增TestIO

---
+++ src/main/java/TestIO.java
@@ -0,0 +1,12 @@
+package src.main.java;
+
+/**
+ * @author suyp
+ * @date 2024-12-16
+ * @description
+ */
+public class TestIO {
+    public static void main(String[] args) {
+        System.out.println("当前是IO类");
+    }
+}

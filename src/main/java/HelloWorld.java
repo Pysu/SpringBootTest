9ead2ad4351f10f679c54f187630c5c6800280ff
suyoupeng <suyoupeng@agree.com.cn>
2024-12-16 11:30:32 +0800

    修改新类

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -8,5 +8,6 @@ package src.main.java;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("What are you doing!");
     }
 }

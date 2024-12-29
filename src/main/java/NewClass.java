120964f736d46b288d226f3090ed14e9a0992f40
suyoupeng <suyoupeng@agree.com.cn>
2024-12-29 21:26:34 +0800

    begin

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -7,10 +7,5 @@ package src.main.java;
  */
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("new Class");
-        System.out.println("change work");
-        System.out.println("end work");
-        System.out.println("new Work");
-        System.out.println("new Work");
     }
 }
